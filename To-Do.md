e854089de2bac04a5377b19b6d661bbe2ca969a0
DOMONIC GONZALES <drpepper6656@gmail.com>
2026-06-18 16:34:33 -0600

    Added code, files, etc

---
+++ To-Do.md
@@ -0,0 +1,4 @@
+## To-Do List
+
+* Create CSS layout for visual appeal. (mobile first approach)
+* Change hardcoded users, JS will be used to update the names, action etc
---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById('notif-1-name').textContent = 'Mark Webber';
+
+const notification = [
+    {
+        id: 1,
+        // more code
+    },
+    {
+        id: 2,
+        //  more code
+    }
+]
